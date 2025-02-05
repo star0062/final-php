b9e8f9a6b5be77b4e4128adb8b3098931d61d684
star <najmeldeen.adamabkar@ynov.com>
2025-02-05 16:58:45 +0100

    db ok connexion ok

---
+++ superheroApi/app/Models/Superhero.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Superhero extends Model
+{
+    use HasFactory;
+    protected $table = 'superheroes';
+                         // 1       2       3         4                5              6               7          8     9
+    protected $fillable = ['name', 'sex', 'word', 'description', 'superpower', 'cityProtection', 'gadgets' , 'team', 'car'];
+} 
