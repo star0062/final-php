28a62dc7b616fc95b16bfa12c0f093546a6b095f
RomainJlt <romainjollivet0@gmail.com>
2025-02-24 13:49:05 +0100

    route get

--- superheroApi/routes/web.php
+++ superheroApi/routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TableSuperheroController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 Route::get('/dashboard', function () {
